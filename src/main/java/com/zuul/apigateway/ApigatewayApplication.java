b26d94a25a2f673f95fc9cf4cd99f8c299ba0b06
lalitrajput72 <lalitrajput72@gmail.com>
2020-09-20 18:13:35 +0530

    Update

--- src/main/java/com/zuul/apigateway/ApigatewayApplication.java
+++ src/main/java/com/zuul/apigateway/ApigatewayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.zuul.apigateway.filters.ErrorFilter;
+import com.zuul.apigateway.filters.PostFilter;
+import com.zuul.apigateway.filters.PreFilter;
+import com.zuul.apigateway.filters.RouteFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -13,5 +19,22 @@ public class ApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
+	
+	 @Bean
+	    public PreFilter preFilter() {
+	        return new PreFilter();
+	    }
+	    @Bean
+	    public PostFilter postFilter() {
+	        return new PostFilter();
+	    }
+	    @Bean
+	    public ErrorFilter errorFilter() {
+	        return new ErrorFilter();
+	    }
+	    @Bean
+	    public RouteFilter routeFilter() {
+	        return new RouteFilter();
+	    }
 
 }
